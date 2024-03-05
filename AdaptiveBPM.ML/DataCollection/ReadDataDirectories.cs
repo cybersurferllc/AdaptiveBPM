e16a62b82fca32c3fc65234b3df11d873a6a6c10
CalebVaccaro <calebvbusiness@gmail.com>
2024-03-05 01:53:10 -0500

    Add ReadDataDirectories.cs

---
+++ AdaptiveBPM.ML/DataCollection/ReadDataDirectories.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+
+namespace AdaptiveBpm.ML.DataCollection;
+
+public static class ReadDataDirectories
+{
+    private static readonly string DirectoryPath = Directory.GetCurrentDirectory();
+    private static readonly string JsonFilePath = Path.Combine(DirectoryPath,"../../../datadirectories.json");
+
+    public static readonly DataDirectory DataDirectory = LoadDataDirectories();
+
+    public static readonly string MasterDataDirectory = DataDirectory.MasterData;
+    public static readonly string MasterModelDirectory = DataDirectory.ModelData;
+    public static readonly string NewDataDirectory = DataDirectory.NewData;
+    public static readonly string UnityDirectory = DataDirectory.UnityDirectory;
+    
+    private static DataDirectory LoadDataDirectories()
+    {
+        try
+        {
+            if (File.Exists(JsonFilePath))
+            {
+                return JsonConvert.DeserializeObject<DataDirectory>(File.ReadAllText(JsonFilePath));
+            }
+            else
+            {
+                // Handle the case when the file does not exist
+                Console.WriteLine($"Error: File not found - {DirectoryPath}");
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle exceptions, e.g., file not accessible
+            Console.WriteLine($"Error reading JSON file: {ex.Message}");
+            return null;
+        }
+    }
+}
+
+public class DataDirectory
+{
+    public string MasterData { get; set; }
+    public string ModelData { get; set; }
+    public string NewData { get; set; }
+    public string UnityDirectory { get; set; }
+}
